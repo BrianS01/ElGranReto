79e3d34fa14d8c388e3abc96b63b43faafc69043
BrianS01 <brian.sterling@correo.usa.edu.co>
2018-05-31 23:41:00 -0500

    Alteración

--- src/main/java/co/edu/usa/ingesoft2/granreto/IFachadaGranReto.java
+++ src/main/java/co/edu/usa/ingesoft2/granreto/IFachadaGranReto.java
@@ -5,18 +5,13 @@ package co.edu.usa.ingesoft2.granreto;
 
 import java.util.List;
 
-/**
- * Ver especificación de los métodos en el enunciado del proyecto.  No cambie nada de este archivo, le va a jugar en contra.
- *
- */
-public interface IFachadaGranReto {
-	
+//Ver especificación de los métodos en el enunciado del proyecto.  No cambie nada de este archivo, le va a jugar en contra.
 
-    void cargarArchivo( String rutaArchivo ) throws GranRetoException;
+public interface IFachadaGranReto
+{
+	void cargarArchivo( String rutaArchivo ) throws GranRetoException;
     
-    List<String> obtenerArticulosMasVendidos( String fechaInicial, String fechaFinal ) 
-            throws GranRetoException;
+    List<String> obtenerArticulosMasVendidos( String fechaInicial, String fechaFinal ) throws GranRetoException;
     
     List<String> obtenerHistorialVentaArticulo( String articulo ) throws GranRetoException;
-    
 }
