ee0b31755fe29ba9dc2273b46a31b24ff1bc9cfc
BrianS01 <brian.sterling@correo.usa.edu.co>
2018-06-01 21:35:58 -0500

    Alteración

--- src/main/java/co/edu/usa/ingesoft2/granreto/GranRetoException.java
+++ src/main/java/co/edu/usa/ingesoft2/granreto/GranRetoException.java
@@ -8,6 +8,7 @@ package co.edu.usa.ingesoft2.granreto;
  * No cambie nada de esta clase.
  *
  */
+
 public class GranRetoException extends Exception
 {
     private static final long serialVersionUID = -3505877527029106577L;
