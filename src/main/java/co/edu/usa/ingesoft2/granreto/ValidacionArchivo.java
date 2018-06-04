290fa2c31a45f39b7d30aaccd4a62a25b64cff85
BrianS01 <brian.sterling@correo.usa.edu.co>
2018-06-04 00:05:35 -0500

    Alteración

--- src/main/java/co/edu/usa/ingesoft2/granreto/ValidacionArchivo.java
+++ src/main/java/co/edu/usa/ingesoft2/granreto/ValidacionArchivo.java
@@ -84,7 +84,8 @@ public class ValidacionArchivo
 	}
 
 	
-	public void validarArchivoVacio(boolean estaLleno) {
+	public void validarArchivoVacio(boolean estaLleno)
+	{
 
 		if (!estaLleno) {
 			errores.add("El archivo no puede estar vacio");
