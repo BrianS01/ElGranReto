4f937fbdd37f52fb23eaae5d6f52e38f29bdea5c
BrianS01 <brian.sterling@correo.usa.edu.co>
2018-06-07 21:48:04 -0500

    Alteración

--- src/main/java/co/edu/usa/ingesoft2/granreto/ValidacionArchivo.java
+++ src/main/java/co/edu/usa/ingesoft2/granreto/ValidacionArchivo.java
@@ -282,7 +282,7 @@ public class ValidacionArchivo
 			if(!(cantidad>0.0 && cantidad<=1000000.0))
 			{
 				errores.add("El rango debe ser mayor a 0,0 y menor o igual a 1000000,0 " + "y se ingreso "+linea.trim());
-		}
+			}
 		}catch(Exception e) {
 			errores.add("El formato de cantidad es incorrecto "+linea.trim());
 		}
