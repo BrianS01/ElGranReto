9f0bb19091284b6154fdfb008c680fa15dca5ff3
BrianS01 <brian.sterling@correo.usa.edu.co>
2018-06-07 21:43:54 -0500

    Alteración

--- src/main/java/co/edu/usa/ingesoft2/granreto/ValidacionArchivo.java
+++ src/main/java/co/edu/usa/ingesoft2/granreto/ValidacionArchivo.java
@@ -274,7 +274,8 @@ public class ValidacionArchivo
 	public void validarRangoCantidad(String linea)
 	{
 		double cantidad=0;
-		try {
+		
+		try{
 		cantidad = Double.parseDouble(linea.trim().replace(",", "."));
 		if(!(cantidad>0.0 && cantidad<=1000000.0)) {
 			errores.add("El rango debe ser mayor a 0,0 y menor o igual a 1000000,0 "
